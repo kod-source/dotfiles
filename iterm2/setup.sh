c033c28008ecaf5b2d174c61b3bc28ff88f2d0bc
kod-source <kodenginner4521@gmail.com>
2022-07-06 21:23:15 +0900

    一旦ファイルの削除

--- iterm2/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ue
-
-CURRENT_DIR=$(cd "$(dirname "$0")"; pwd)
-
-ln -fs "$CURRENT_DIR/com.googlecode.iterm2.plist" ~/Library/Preferences
--- rectangle/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ue
-
-CURRENT_DIR=$(cd "$(dirname "$0")"; pwd)
-
-ln -fs "$CURRENT_DIR/com.knollsoft.Rectangle.plist" ~/Library/Preferences
