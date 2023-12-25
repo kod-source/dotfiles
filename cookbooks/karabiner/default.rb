d51144bbdd3858ba112f5ea32526991ac33721c7
堀航大 <kodenginner4521@gmail.com>
2023-12-26 02:25:59 +0900

    fix

--- cookbooks/karabiner/default.rb
+++ cookbooks/karabiner/default.rb
@@ -1,34 +1,10 @@
 darwin_app 'karabiner-elements'
 
+Dir.glob(File.expand_path('../templates/**/*', __FILE__)).each do |template_file|
+    next if File.directory?(template_file)
 
-link File.expand_path("#{ENV['HOME']}/.config/karabiner/karabiner.json") do
-    to File.expand_path('../templates/karabiner.json', __FILE__)
-    force true
+    link File.join(ENV['HOME'], '.config', 'karabiner', template_file.gsub(File.expand_path('../templates', __FILE__), '')) do
+        to template_file
+        force true
+    end
 end
-
-# Dir.glob(File.expand_path('templates/**/*')).each do |src_file|
-#     next unless File.file?(src_file)  # Skip directories
-#     dest_file = File.join(ENV['HOME'], '.config', 'karabiner', File.relative_path('templates', src_file))
-#     directory File.dirname(dest_file) do
-#         action :create
-#         recursive true
-#     end
-
-#     template dest_file do
-#         source src_file
-#     end
-# end
-
-# template "#{ENV['HOME']}/.config/karabiner/karabiner.json" do
-#     source "templates/karabiner.json"
-# end
-
-# template "#{ENV['HOME']}/.config/karabiner/assets/complex_modifications/1648355185.json" do
-#     source "templates/1648355185.json"
-# end
-# template "#{ENV['HOME']}/.config/karabiner/assets/complex_modifications/setting.json" do
-#     source "templates/setting.json"
-# end
-# template "#{ENV['HOME']}/.config/karabiner/assets/complex_modifications/vim.json" do
-#     source "templates/vim.json"
-# end
