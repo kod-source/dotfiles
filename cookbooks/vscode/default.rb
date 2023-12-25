26df563a113e6c0cf430415f7d107cbf9eb0b815
kod-source <kodai.hori@fuller.co.jp>
2023-12-25 18:43:50 +0900

    fix

--- cookbooks/vscode/default.rb
+++ cookbooks/vscode/default.rb
@@ -17,15 +17,32 @@ template "#{ENV['HOME']}/Library/Application Support/Code/User/keybindings.json"
   source "templates/keybindings.json"
 end
 
+# execute "install extensions" do
+#   command <<-EOC
+#     if [ -x "$(command -v code)" ]; then
+#         cat < "templates/extensions" | while read -r line
+#         do
+#             code --install-extension "$line"
+#         done
+
+#         code --list-extensions > "templates/extensions"
+#     else
+#         echo "VSCode is not installed."
+#     fi
+#   EOC
+# end
+
+cookbook_dir = File.dirname(__FILE__)
+
 execute "install extensions" do
   command <<-EOC
     if [ -x "$(command -v code)" ]; then
-        cat < "templates/extensions" | while read -r line
+        cat < "#{cookbook_dir}/templates/extensions" | while read -r line
         do
             code --install-extension "$line"
         done
 
-        code --list-extensions > "templates/extensions"
+        code --list-extensions > "#{cookbook_dir}/templates/extensions"
     else
         echo "VSCode is not installed."
     fi
