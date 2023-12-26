20568031bd1a81ade6cce945c134d65306926118
kod-source <kodai.hori@fuller.co.jp>
2023-12-26 10:18:46 +0900

    fix

--- cookbooks/fish-shell/default.rb
+++ cookbooks/fish-shell/default.rb
@@ -1,16 +1,6 @@
 package 'fish'
 fish_path = '/opt/homebrew/bin/fish'
 
-Dir.glob(File.expand_path('../templates/**/*', __FILE__)).each do |template_file|
-    next if File.directory?(template_file)
-
-    link File.join(ENV['HOME'], '.config', 'fish', template_file.gsub(File.expand_path('../templates', __FILE__), '')) do
-        to template_file
-        force true
-    end
-end
-
-
 execute "echo #{fish_path} | sudo tee -a /etc/shells" do
     not_if "grep #{fish_path} /etc/shells"
 end
@@ -21,4 +11,13 @@ end
 
 execute 'curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish' do
     not_if "test -f #{ENV['HOME']}/.config/fish/functions/fisher.fish"
+end
+
+Dir.glob(File.expand_path('../templates/**/*', __FILE__)).each do |template_file|
+    next if File.directory?(template_file)
+
+    link File.join(ENV['HOME'], '.config', 'fish', template_file.gsub(File.expand_path('../templates', __FILE__), '')) do
+        to template_file
+        force true
+    end
 end
