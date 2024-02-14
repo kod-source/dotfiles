ace0f3c62052ba9a5018fa34323ef97d50be6626
kod-source <kodai.hori@fuller.co.jp>
2024-02-14 18:54:46 +0900

    fix tempalte

--- cookbooks/asdf/default.rb
+++ cookbooks/asdf/default.rb
@@ -1,28 +1,21 @@
-package 'gpg'
 package 'asdf'
 
-[
-    'ruby https://github.com/asdf-vm/asdf-ruby.git',
-    'golang https://github.com/asdf-community/asdf-golang.git',
-    'nodejs https://github.com/asdf-vm/asdf-nodejs.git'
-].each do |pkg|
-    name, url = pkg.split
-    execute "asdf plugin add #{name}" do
-        command "asdf plugin add #{name} #{url}"
-        not_if "asdf plugin list | grep -q #{name}"
-    end
+link File.expand_path("#{ENV['HOME']}/.tool-versions") do
+    to File.expand_path('../templates/.tool-versions', __FILE__)
+    force true
 end
 
-[
-    'ruby',
-    'golang',
-    'nodejs'
-].each do |pkg|
-    execute "asdf install #{pkg} latest" do
-        command <<-EOC
-            VERSION=$(asdf latest #{pkg})
-            asdf install #{pkg} $VERSION
-            asdf global #{pkg} $VERSION
-        EOC
+execute "install asdf" do
+    command <<-EOC
+        asdf install
+    EOC
+end
+
+File.open("#{ENV['HOME']}/.tool-versions") do |file|
+    file.each_line do |line|
+        pkg, version = line.split
+        execute "asdf global #{pkg} #{version}" do
+            command "asdf global #{pkg} #{version}"
+        end
     end
 end
