c1c0952a4e4cc850e8a4951391565545a9da1942
kod-source <74532799+kod-source@users.noreply.github.com>
2024-05-29 09:22:02 +0900

    fix ide script (#12)

--- scripts/ide.sh
+++ scripts/ide.sh
@@ -12,15 +12,16 @@ done
 
 if [ "$#" -eq 0 ]; then
     tmux split-window -h
-    tmux split-window -v -p 30
+    tmux split-window -v
     tmux split-window -t 0 -v
+    tmux resize-pane -t 0 -D 7
+    tmux resize-pane -t 2 -D 15
     tmux resize-pane -R 10
     sleep 1
-    tmux send-keys -t 0 "ghq_peco_repo; clear" C-m
-    tmux send-keys -t 1 "ghq_peco_repo; clear" C-m
-    tmux send-keys -t 2 "ghq_peco_repo; clear" C-m
-    tmux send-keys -t 3 "ghq_peco_repo; clear; lazygit" C-m
-    tmux select-pane -t 0
+    tmux send-keys -t 0 "ghq_peco_repo; tmux select-pane -t 1; clear" C-m
+    tmux send-keys -t 1 "ghq_peco_repo; tmux select-pane -t 2; clear" C-m
+    tmux send-keys -t 2 "ghq_peco_repo; tmux select-pane -t 3; clear" C-m
+    tmux send-keys -t 3 "ghq_peco_repo; tmux select-pane -t 0; clear; lazygit" C-m
 else
     case $1 in
     1)
