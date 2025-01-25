e093f417512a0c436c14179bb77f3dea186dd8ad
kod-source <kodenginner4521@gmail.com>
2025-01-25 23:48:02 +0900

    fix

--- cookbooks/asdf/default.rb
+++ cookbooks/asdf/default.rb
@@ -2,8 +2,6 @@ execute "install" do
     command <<-EOC
         git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
         mkdir -p ~/.config/fish/completions; ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
-        echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
-        # echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
         echo '. "$HOME/.asdf/asdf.sh"' >> ~/.zshrc
     EOC
     not_if { ::File.exist?(File.expand_path("~/.asdf")) }
@@ -14,26 +12,24 @@ link File.expand_path("#{ENV['HOME']}/.tool-versions") do
     force true
 end
 
-# if ENV['GITHUB_ACTIONS'] != 'true'
-    [
-        'ruby https://github.com/asdf-vm/asdf-ruby.git',
-        'golang https://github.com/asdf-community/asdf-golang.git',
-        'nodejs https://github.com/asdf-vm/asdf-nodejs.git',
-        'pnpm https://github.com/jonathanmorley/asdf-pnpm'
-    ].each do |pkg|
-        name, url = pkg.split
-        execute "asdf plugin add #{name}" do
-            command "asdf plugin add #{name} #{url}"
-            not_if "asdf plugin list | grep -q #{name}"
-        end
+[
+    'ruby https://github.com/asdf-vm/asdf-ruby.git',
+    'golang https://github.com/asdf-community/asdf-golang.git',
+    'nodejs https://github.com/asdf-vm/asdf-nodejs.git',
+    'pnpm https://github.com/jonathanmorley/asdf-pnpm'
+].each do |pkg|
+    name, url = pkg.split
+    execute "asdf plugin add #{name}" do
+        command "asdf plugin add #{name} #{url}"
+        not_if "asdf plugin list | grep -q #{name}"
     end
+end
 
-    execute "install asdf" do
-        command <<-EOC
-            asdf install
-        EOC
-    end
-# end
+execute "install asdf" do
+    command <<-EOC
+        asdf install
+    EOC
+end
 
 File.open("#{ENV['HOME']}/.tool-versions") do |file|
     file.each_line do |line|
