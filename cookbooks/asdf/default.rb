ad5a2004997b5d3a37003036b55a7517d0692ec6
kod-source <kodenginner4521@gmail.com>
2025-01-25 18:50:29 +0900

    asdfの設定はCI上では行わない

--- cookbooks/asdf/default.rb
+++ cookbooks/asdf/default.rb
@@ -14,24 +14,26 @@ link File.expand_path("#{ENV['HOME']}/.tool-versions") do
     force true
 end
 
-[
-    'ruby https://github.com/asdf-vm/asdf-ruby.git',
-    'golang https://github.com/asdf-community/asdf-golang.git',
-    'nodejs https://github.com/asdf-vm/asdf-nodejs.git'
-].each do |pkg|
-    name, url = pkg.split
-    execute "asdf plugin add #{name}" do
-        command "asdf plugin add #{name} #{url}"
-        not_if "asdf plugin list | grep -q #{name}"
+if ENV['GITHUB_ACTIONS'] != 'true'
+    [
+        'ruby https://github.com/asdf-vm/asdf-ruby.git',
+        'golang https://github.com/asdf-community/asdf-golang.git',
+        'nodejs https://github.com/asdf-vm/asdf-nodejs.git',
+        'pnpm https://github.com/jonathanmorley/asdf-pnpm'
+    ].each do |pkg|
+        name, url = pkg.split
+        execute "asdf plugin add #{name}" do
+            command "asdf plugin add #{name} #{url}"
+            not_if "asdf plugin list | grep -q #{name}"
+        end
+    end
+
+    execute "install asdf" do
+        command <<-EOC
+            asdf install
+        EOC
     end
 end
-
-execute "install asdf" do
-    command <<-EOC
-        asdf install
-    EOC
-end
-
 File.open("#{ENV['HOME']}/.tool-versions") do |file|
     file.each_line do |line|
         pkg, version = line.split
