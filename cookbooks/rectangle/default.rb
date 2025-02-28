61c83907401472c4209fafd2ca992f02088e3f89
kod-source <kodenginner4521@gmail.com>
2025-02-28 14:36:43 +0900

    Rectangleの設定の修正

--- cookbooks/rectangle/default.rb
+++ cookbooks/rectangle/default.rb
@@ -1,6 +1,1 @@
 darwin_app 'rectangle'
-
-link File.expand_path("#{ENV['HOME']}/Library/Preferences/com.knollsoft.Rectangle.plist") do
-    to File.expand_path('../templates/com.knollsoft.Rectangle.plist', __FILE__)
-    force true
-end
